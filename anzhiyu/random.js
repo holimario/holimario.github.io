fa1a4331fab1804319eb73a936a69aa17739dcf2
holimario <1437273841@qq.com>
2025-02-09 19:40:53 +0800

    Site updated: 2025-02-09 19:40:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/07/hello-world/","2025/02/08/missing-semester-2-shell-gong-ju-he-jiao-ben/","2025/02/07/missing-semester-1-shell/"];function toRandomPost(){
+var posts=["2025/02/07/hello-world/","2025/02/07/missing-semester-1-shell/","2025/02/08/missing-semester-2-shell-gong-ju-he-jiao-ben/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
