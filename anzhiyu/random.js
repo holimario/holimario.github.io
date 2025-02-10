faff1f30ca9e4541707c1fa4b56df7f8cfe673d6
holimario <1437273841@qq.com>
2025-02-10 20:20:21 +0800

    Site updated: 2025-02-10 20:20:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/07/undefined.html","2025/02/07/undefined.html","2025/02/08/undefined.html","2025/02/09/undefined.html"];function toRandomPost(){
+var posts=["2025/02/07/undefined.html","2025/02/09/undefined.html","2025/02/07/undefined.html","2025/02/08/undefined.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
