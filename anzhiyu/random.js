13e402db93fe8b8318bba014943c3e5bb3624700
holimario <1437273841@qq.com>
2025-02-11 00:33:54 +0800

    Site updated: 2025-02-11 00:33:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/07/4a17b156.html","2025/02/07/fa4b86b9.html","2025/02/09/86d7b9fc.html","2025/02/08/8d29a533.html"];function toRandomPost(){
+var posts=["2025/02/07/4a17b156.html","2025/02/07/fa4b86b9.html","2025/02/08/8d29a533.html","2025/02/09/86d7b9fc.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
