24b66fe9dd8b7970e337b552724ba781dbcf603b
holimario <1437273841@qq.com>
2025-02-13 23:41:40 +0800

    Site updated: 2025-02-13 23:41:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/07/4a17b156.html","2025/02/07/fa4b86b9.html","2025/02/08/8d29a533.html","2025/02/11/4cc7c60c.html","2025/02/09/86d7b9fc.html","2025/02/12/aa9eecf8.html"];function toRandomPost(){
+var posts=["2025/02/07/4a17b156.html","2025/02/07/fa4b86b9.html","2025/02/08/8d29a533.html","2025/02/11/4cc7c60c.html","2025/02/09/86d7b9fc.html","2025/02/12/aa9eecf8.html","2025/02/12/81b3bf3b.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
