77506df3743fa521f52466babb42b1cccc63ff6c
holimario <1437273841@qq.com>
2025-03-28 13:23:21 +0800

    Site updated: 2025-03-28 13:23:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/20/ba24de2.html","2025/03/18/1f1ed4e9.html","2025/02/12/aa9eecf8.html","2025/02/07/4a17b156.html","2025/02/07/fa4b86b9.html","2025/02/08/8d29a533.html","2025/02/12/81b3bf3b.html","2025/03/18/a872bcc1.html","2025/02/09/86d7b9fc.html","2025/03/05/31689640.html","2025/02/14/89184582.html","2025/02/23/1192c262.html","2025/02/11/4cc7c60c.html","2025/02/23/3abf91a1.html","2025/02/25/23a4a0e0.html"];function toRandomPost(){
+var posts=["2025/03/20/ba24de2.html","2025/02/07/fa4b86b9.html","2025/02/07/4a17b156.html","2025/02/08/8d29a533.html","2025/02/09/86d7b9fc.html","2025/02/12/81b3bf3b.html","2025/03/18/1f1ed4e9.html","2025/02/12/aa9eecf8.html","2025/02/14/89184582.html","2025/03/05/31689640.html","2025/02/11/4cc7c60c.html","2025/03/18/a872bcc1.html","2025/02/25/23a4a0e0.html","2025/02/23/1192c262.html","2025/02/23/3abf91a1.html"];function toRandomPost(){
     window.location.href='/'+posts[Math.floor(Math.random() * posts.length)];
   };
